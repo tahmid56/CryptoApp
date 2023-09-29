16c3d6046d1777019a4af28a10463bc6c9ea99ea
Tahmid Akter <tahmidakter56@gmail.com>
2023-09-29 06:21:19 +0600

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Tahmid Akter on 29/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
